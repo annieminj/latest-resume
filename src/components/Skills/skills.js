f6c6e1b4dbf25835edb1990f9c3abfbaebcd06f0
annieminj <156908924+annieminj@users.noreply.github.com>
2024-02-18 00:25:53 +0530

    added progress bars

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,8 +1,72 @@
 import React from 'react'
+import './skills.css'
+import html from '../../assets/html.png'
+import css from '../../assets/css.jpg'
+import javascript from '../../assets/javascript.png'
+import react from '../../assets/react.png'
+import angular from '../../assets/angular.jpg'
+import ionic from '../../assets/ioic.png'
+import Progress_bar from '../../utilities/progress-bar'
 
 const Skills = () => {
   return (
-    <div>skills</div>
+    <section className='skills' id='skills'>
+        <span className='skillsTitle'>My Skills</span>
+        <span className='skillsDescription'>
+          <div>
+            <div className='skillList'>
+              <span className='skillLogo'><img className='skillImg' src={html} alt='image' /></span>
+              <span className='skillHead'>HTML</span><br/>
+            </div>
+            <div className='skillList'>
+              <span className='skillLogo'><img className='skillImg' src={css} alt='image' /></span>
+              <span className='skillHead'>CSS</span><br/>
+            </div>
+            <div className='skillList'>
+              <span className='skillLogo'><img className='skillImg' src={javascript} alt='image' /></span>
+              <span className='skillHead'>Javascript</span><br/>
+            </div>
+            <div className='skillList'>
+              <span className='skillLogo'><img className='skillImg' src={react} alt='image' /></span>
+              <span className='skillHead'>React</span><br/>
+            
+            </div>
+            <div className='skillList'>
+              <span className='skillLogo'><img className='skillImg' src={angular} alt='image' /></span>
+              <span className='skillHead'>Angular</span><br/>
+            </div>
+            <div className='skillList'>
+              <span className='skillLogo'><img className='skillImg' src={ionic} alt='image' /></span>
+              <span className='skillHead'>Ionic</span><br/>
+            </div>
+          </div>
+          <div className='skillsBox'>
+            <span>
+              <Progress_bar progress="85"/>
+            </span>
+            <span>
+              <Progress_bar progress="90"/>
+            </span>
+            <span>
+              <Progress_bar progress="80"/>
+            </span>
+            <span>
+              <Progress_bar progress="50"/>
+            </span>
+            <span>
+              <Progress_bar progress="80"/>
+            </span>
+            <span>
+                <Progress_bar progress="70"/>
+              </span>
+          </div>
+
+          
+          
+          
+          
+        </span>
+    </section>
   )
 }
 
---
+++ src/utilities/progress-bar.js
@@ -0,0 +1,42 @@
+// Filename - Progress_bar.js
+
+import React from 'react'
+
+const Progress_bar = ({progress}) => {
+	
+	const Parentdiv = {
+		height: 13,
+		width: '100%',
+		backgroundColor: 'whitesmoke',
+		borderRadius: 40,
+		margin: 50,
+        marginBottom: 42,
+        marginTop: 42
+	}
+	
+	const Childdiv = {
+		height: '100%',
+		width: `${progress}%`,
+		backgroundColor: 'yellowgreen',
+	    borderRadius:40,
+		textAlign: 'right'
+	}
+	
+	const progresstext = {
+		fontWeight: 500,
+        padding: 10,
+        color: 'white',
+        position: 'absolute',
+        marginTop: '5'
+	}
+	
+	return (
+	<div style={Parentdiv}>
+	<div style={Childdiv}>
+		<span style={progresstext}>{`${progress}%`}</span>
+	</div>
+	</div>
+	)
+}
+
+export default Progress_bar;
