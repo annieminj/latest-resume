73920100958a0cbb5911999d5905dfc09d396c18
annieminj <156908924+annieminj@users.noreply.github.com>
2024-02-17 15:17:33 +0530

    modified

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './intro.css'
-import person from '../../assets/16.jpeg'
+import person from '../../assets/18.png'
 import {Link} from 'react-scroll'
 import Typewriter from "typewriter-effect";
+import resume from "../../assets/Annie_Minj.pdf"
 
 const Intro = () => {
+
+  const downloadCV = () => {
+    console.log("downloadCV")
+    const pdfUrl = {resume}
+      const link = document.createElement("a");
+      link.href = pdfUrl;
+      link.download = "document.pdf"; // specify the filename
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+  }
+
   return (
     <section className='introHead' id='intro'>
         <div className='introContent'>
@@ -23,8 +36,8 @@ const Intro = () => {
                   // }}
               />
             </span>
-            <p className='introPara'>I am a skilled Front-end Developer with experience in designing, developing <br/> and maintaining user friendly front-end web and mobile applications </p>
-            <Link><button className='btn'>Hire Me</button></Link>
+            <p className='introPara'>I am a skilled Front-end Developer with experience in designing, developing <br/> and maintaining user friendly web and mobile applications </p>
+            <button className='btn' onClick={() => downloadCV()}>Download CV</button>
         </div>
         <img src={person} alt='image' className='bg'/>
     </section>
--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -7,27 +7,33 @@ const Skills = () => {
   return (
     <section className='skills' id='skills'>
         <span className='skillTitle'>What I do</span>
-        <span className='skillDescription'>I am a skilled and passionate web designer with experience of 3.5 years in HTMl, CSS and experince of 3 years in React, Angular and Ionic framework.</span>
+        <span className='skillDescription'>I'm a seasoned professional with over 3 years of experience in the industry. 
+                My passion lies in implementing captivating designs through front-end development. 
+                I take pride in producing visually appealing, smooth, interactive and user-friendly applications.
+                I have a proven track record of delivering the best results within a given time.
+                When starting a new project, I thoroughly research and analyse the client's requirements and also
+                I excel at building and maintaining good client relationships.
+        </span>
         <div className='skillBars'>
             <div className='skillBar'>
                 <img className='skillBarImg' src={UIDesign} alt='UIDesign' />
                 <div className='skillBarText'>
                     <h2>UI/UX Design</h2>
-                    <p>This is a demo text, you can write your own content here</p>
+                    <p>Designing Web/App interfaces</p>
                 </div>
             </div>
             <div className='skillBar'>
                 <img className='skillBarImg' src={WebDesign} alt='WebDesign' />
                 <div className='skillBarText'>
-                    <h2>Website Design</h2>
-                    <p>This is a demo text, you can write your own content here</p>
+                    <h2>Web Development</h2>
+                    <p>Web app Development</p>
                 </div>
             </div>
             <div className='skillBar'>
-                <img className='skillBarImg' src={UIDesign} alt='UIDesign' />
+                <img className='skillBarImg' src={WebDesign} alt='WebDesign' />
                 <div className='skillBarText'>
-                    <h2>UI/UX Design</h2>
-                    <p>This is a demo text, you can write your own content here</p>
+                    <h2>App Development</h2>
+                    <p>Building Android/iOS apps</p>
                 </div>
             </div>
         </div>
